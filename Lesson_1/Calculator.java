6242600747b6121bcde004b64964f1eaf3429a25
ilyxer999 <shishkin.is@mail.ru>
2024-01-14 00:30:44 +1000

    modified Calculator.java, MyFirstGame.java

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,22 +1,21 @@
 public class Calculator {
     public static void main(String[] args) {
         int a = 8;
-        int b = 4;
-        char sign = '=';
+        int b = 3;
+        char sign = '%';
         int result = 0;
         if (a < 1 || b < 1) {
             System.out.println("Введенные числа должны быть положительными!");
             return;
-        } else if (sign == '+') {
+        }
+        if (sign == '+') {
             result = a + b;
         } else if (sign == '-') {
             result = a - b;
         } else if (sign == '*') {
             result = a * b;
         } else if (sign == '/') {
-            double divisionResult = (double) a / b;
-            System.out.println(a + " " + sign + " " + b + " = " + divisionResult);
-            return;
+            result = 8 / 4;
         } else if (sign == '^') {
             result = a;
             for (int i = 2; i <= b; i++) {
--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,19 +2,16 @@ public class MyFirstGame {
     public static void main(String[] args) {
         int computerNum = 67;
         int playerNum = 54;
-        if (computerNum > 100 || computerNum < 1) {
+        if (computerNum < 1 || computerNum > 100) {
             System.out.println("Измените число, которое необходимо отгадать");
         } else {
             while (computerNum != playerNum) {
                 if (playerNum > computerNum) {
                     System.out.printf("Число %d больше того, что загадал компьютер\n", playerNum);
+                    playerNum -= 3;
                 } else {
                     System.out.printf("Число %d меньше того, что загадал компьютер\n", playerNum);
-                }
-                if (playerNum < computerNum) {
                     playerNum += 5;
-                } else {
-                    playerNum -=3;
                 }
             }
         }
