4c291b980fb6241d75c4a6426270c3aff6d7f020
ilyxer999 <shishkin.is@mail.ru>
2024-01-15 00:37:42 +1000

    add Lesson_2 folder & Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    String gender = "мужчина";
+    String name = "Алик";
+    int height = 180;
+    double weight = 85.5;
+    int age = 27;
+
+    void go() {
+        System.out.println("Идет");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void say() {
+        System.out.println("Говорит");
+    }
+
+    void learn() {
+        System.out.println("Учит Java");
+    }
+    
+}
